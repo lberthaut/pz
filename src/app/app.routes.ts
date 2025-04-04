e71169e3c31f9294e64306b27347b7ca1c792584
Louis Berthaut <louis.berthaut@gmx.com>
2025-04-04 21:18:54 +0200

    montage de l'accueil / responsive en cours

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,12 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
+import { HomeComponent } from './pages/home/home.component';
+import { TeamComponent } from './pages/team/team.component';
 // import { HomeComponent } from './pages/home/home.component';
 // import { AboutComponent } from './pages/about/about.component';
 // import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  //   { path: 'about', component: AboutComponent },
+  { path: 'team', component: TeamComponent },
   //   { path: '**', component: NotFoundComponent }
 ];
