a34d70b461b8f007890c4821d5bb2f2daf1fdd07
Louis Berthaut <louis.berthaut@gmx.com>
2025-04-04 19:01:03 +0200

    montage de l'accueil

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 // import { HomeComponent } from './pages/home/home.component';
 // import { AboutComponent } from './pages/about/about.component';
 // import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const routes: Routes = [
-  //   { path: '', component: HomeComponent },
+  { path: '', component: HomeComponent },
   //   { path: 'about', component: AboutComponent },
   //   { path: '**', component: NotFoundComponent }
 ];
