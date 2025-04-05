3c70dcb7b2d0cbb45c01922f718cedb6c8ca7e2d
Louis Berthaut <louis.berthaut@gmx.com>
2025-04-05 16:08:42 +0200

    mise en place du routerlink

--- src/app/pages/notfound404/notfound404.component.ts
+++ src/app/pages/notfound404/notfound404.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-notfound404',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './notfound404.component.html',
-  styleUrl: './notfound404.component.scss'
+  styleUrl: './notfound404.component.scss',
 })
-export class Notfound404Component {
-
-}
+export class Notfound404Component {}
