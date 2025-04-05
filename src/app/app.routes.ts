79a8abcef08aec4fbfb40ab0ec5969d32af54422
Louis Berthaut <louis.berthaut@gmx.com>
2025-04-05 14:47:51 +0200

    ajout page 404

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { TeamComponent } from './pages/team/team.component';
+import { Notfound404Component } from './pages/notfound404/notfound404.component';
 // import { HomeComponent } from './pages/home/home.component';
 // import { AboutComponent } from './pages/about/about.component';
-// import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'team', component: TeamComponent },
-  //   { path: '**', component: NotFoundComponent }
+  { path: '**', component: Notfound404Component },
 ];
---
+++ src/app/pages/notfound404/notfound404.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notfound404',
+  imports: [],
+  templateUrl: './notfound404.component.html',
+  styleUrl: './notfound404.component.scss'
+})
+export class Notfound404Component {
+
+}
